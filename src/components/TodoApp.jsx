b33a4c36d334d2f1c8e90b231b2ced8a3be6bb01
Matty Witney <mattwitney@gmail.com>
2023-01-24 09:18:15 -0600

    I don't understand this

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function TodoApp() {
+    console.log(props);
+  return (
+    <div>TodoApp</div>
+  )
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TodoList(props) {
+    console.log(props)
+  return (
+    <div>TodoList</div>
+    <div>
+        <form>
+        <button></button>
+    </form>
+    </div>
+  )
+}
